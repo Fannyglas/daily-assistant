56df20c091a8cdddd9be2986d61c73e7d3e9613e
Fannyglas <darya.chubik@gmail.com>
2025-02-06 23:32:27 +0500

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,7 @@
+pkg install git
+git clone https://github.com/ТВОЙ-НИК/ТВОЙ-РЕПОЗИТОРИЙ.git
+cd ТВОЙ-РЕПОЗИТОРИЙ
+echo 'print("Hello, GitHub!")' > main.py
+git add .
+git commit -m "Добавлен первый файл"
+git push origin main
